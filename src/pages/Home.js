bf3184d3caacf2872a50f9f8d47e9b180dfaba41
Felipe <felipe.marquez654@gmail.com>
2024-04-09 12:31:47 -0400

    change direcction file hlsData

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function Home (){
         <div className='min-h-screen min-w-full p-6 sm:p-10 bg-gray-100'>
             <div className="mt-6 grid gap-6 sm:grid-cols-1 lg:grid-cols-2">
                 <div >
-                    <VideoPlayer className="bg-white rounded-lg shadow-md overflow-hidden" src={`${cameraRoute}/hlsData/stream.m3u8`} />
+                    <VideoPlayer className="bg-white rounded-lg shadow-md overflow-hidden" src={`${cameraRoute}/hlsDataRut956_1/stream.m3u8`} />
                     <div className=" mt-12 bg-gray-100">
                     <Joystick numberCamera={1}></Joystick>
                     </div>

099ada2e49bc11efafec11eb7b259fca8fc50f44
Felipe <felipe.marquez654@gmail.com>
2024-05-02 15:33:02 -0400

    fixed login with all routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -54,6 +54,19 @@ function Login() {
                         />
                     </div>
                     <div className="flex flex-col space-y-4">
+
+                        <button type="button" onClick={(e) => handleLogin(e, '/home')} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                            WATC001-Cam
+                        </button>
+                        <button type="button" onClick={(e) => handleLogin(e, '/hom2')} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                            WATC002-Cam
+                        </button>
+                        <button type="button" onClick={(e) => handleLogin(e, '/home3')} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                            WATC003-Cam-
+                        </button>
+
+
+
                         <button type="button" onClick={(e) => handleLogin(e, '/secondary')} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
                             WATC001-Cam-Secondary
                         </button>
